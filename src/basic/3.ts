086144dc54a6a8c6657d03e61b2fdf7c900d4c23
Elena Zhuvak <elenkazh@gmail.com>
2025-02-21 19:57:00 +0000

    adds 3.ts

---
+++ src/basic/3.ts
@@ -0,0 +1,12 @@
+let unionType: string | number
+unionType = "Tom"
+unionType = 10
+
+console.log(unionType)
+
+// Literal type
+type Status = "enable" | "disable"
+let value: Status = "enable"
+value = "disable"
+
+console.log(value)
