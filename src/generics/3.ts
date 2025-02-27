f9dee7d87dd9438fe0ef302b0f1febb63f7fc529
Elena Zhuvak <elenkazh@gmail.com>
2025-02-27 22:37:44 +0000

    adds 1.ts, 3.ts, 4.ts

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,24 +1,3 @@
-interface User {
-  name: string;
-  surname: string;
-  email: string;
-  password: string;
-};
-const userProfile = {
-    name: 'Petro',
-    surname: 'Galych',
-    email: 'petro@mail.com',
-    password: 'qwerty123'
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
 }
-console.log(userProfile)
-
-function createOrUpdateUser(initialValues: User, updates: Partial <User>) {
-    return {
-        ...initialValues, ...updates
-    }
-}
-
-console.log(createOrUpdateUser(userProfile, {
-  email: 'user@mail.com',
-  password: 'password123',
-}));
---
+++ src/generics/4.ts
@@ -0,0 +1,25 @@
+interface User {
+    name: string;
+    surname: string;
+    email: string;
+    password: string;
+  };
+  const userProfile = {
+      name: 'Petro',
+      surname: 'Galych',
+      email: 'petro@mail.com',
+      password: 'qwerty123'
+  }
+  console.log(userProfile)
+  
+  function createOrUpdateUser(initialValues: User, updates: Partial <User>) {
+      return {
+          ...initialValues, ...updates
+      }
+  }
+  
+  console.log(createOrUpdateUser(userProfile, {
+    email: 'user@mail.com',
+    password: 'password123',
+  }));
+  
